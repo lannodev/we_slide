b965133b3e3a133a11e74fdce0b238675f9733e6
wefixitbr <12519008+luciano-work@users.noreply.github.com>
2021-01-05 21:29:17 -0300

    removed deprecated properties

--- example/lib/music_app/bottom_bar.dart
+++ example/lib/music_app/bottom_bar.dart
@@ -8,9 +8,10 @@ class BottomBar extends StatelessWidget {
     final colorTheme = Theme.of(context).colorScheme;
     return BottomNavigationBar(
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-        BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
-        BottomNavigationBarItem(icon: Icon(Icons.library_books), title: Text('Your Library')),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.library_books), label: 'Your Library'),
       ],
       elevation: 0,
       backgroundColor: colorTheme.surface,
--- example/lib/store_app/bottom_bar.dart
+++ example/lib/store_app/bottom_bar.dart
@@ -8,9 +8,9 @@ class BottomBar extends StatelessWidget {
     final colorTheme = Theme.of(context).colorScheme;
     return BottomNavigationBar(
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-        BottomNavigationBarItem(icon: Icon(Icons.store), title: Text('Search')),
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), title: Text('Cart')),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.store), label: 'Search'),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
       ],
       elevation: 0,
       backgroundColor: colorTheme.surface,
